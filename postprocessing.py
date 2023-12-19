cb71f4c5487beb5a79e5d05988414878a40903e7
Tuan Nguyen <workwhileweb@gmail.com>
2023-12-19 21:08:21 +0700

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -21,7 +21,7 @@ logger.setLevel(logging.DEBUG)
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://workwhileweb.github.io/tiktok-rss-flat/"
 
 maxItems = 5
 
